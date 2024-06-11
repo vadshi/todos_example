697d18290bedffa75c0581d97dc299b18b3a0156
Vadim <vadimx30@gmail.com>
2024-06-11 11:26:53 +0300

    Add resolvers code

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -14,7 +14,7 @@ type Query struct {
 }
 
 type Todo struct {
-	ID   string `json:"id" bson:"_id"`
+	ID   string `json:"id"`
 	Text string `json:"text"`
 	Done bool   `json:"done"`
 	User *User  `json:"user"`
